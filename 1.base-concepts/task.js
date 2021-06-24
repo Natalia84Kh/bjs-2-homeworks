f7332fe6a98c9d181c70d8e3fd7a5095044eb43d
Natalia84Kh <73022006+Natalia84Kh@users.noreply.github.com>
2021-06-24 22:07:30 +0300

    Add files via upload
    "Добавлено решение"

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,42 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   // код для задачи №1 писать здесь
-  return arr; // array
-}
+ 
+  let D = b ** 2 - 4 * a * c;
+  if (D < 0) {
+    return arr;
+  } else if (D === 0) {
+      let x = - b / (2 * a);
+      arr.push(x);
+      return arr;
+    } else {
+        let x1 = (-b + Math.sqrt(D)) / (2 * a); 
+        let x2 = (-b - Math.sqrt(D)) / (2 * a);
+        arr.push(x1, x2);
+        return arr;
+      }
+};
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  if (parseInt(percent) === NaN) {
+    console.log (`'Параметр "percent" содержит неправильное значение "${percent}"'`);
+  }
+  if (parseInt(contribution) === NaN) {
+    console.log (`Параметр "contribution" содержит неправильное значение "${contribution}"`);
+  }
+  if (parseInt(contribution) === NaN) {
+    console.log (`Параметр "amount" содержит неправильное значение "${amount}"`);
+  }
 
-  return totalAmount;
-}
+  let s =  (amount - contribution);
+  let p = (percent / 12);
+  let today = new Date();
+  let n = (13 - parseInt(today.getMonth())) + parseInt(date.getMonth()) + 1;
+  totalAmount = (s * (p + p / (((1 + p) ** n) - 1))); 
+    
+  return (+totalAmount.toFixed(2));
+};
