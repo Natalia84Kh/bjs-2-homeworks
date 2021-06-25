8f73f5816aff2a6e36c54c40b9ea75b3023e506e
Natalia84Kh <73022006+Natalia84Kh@users.noreply.github.com>
2021-06-25 16:06:33 +0300

    Вторая попытка

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,39 +4,36 @@ function solveEquation(a, b, c) {
   let arr = [];
   // код для задачи №1 писать здесь
  
-  let D = b ** 2 - 4 * a * c;
-  if (D < 0) {
+  const D = b ** 2 - 4 * a * c;
+  if (D === 0) {
+    let x = - b / (2 * a);
+    arr.push(x);
     return arr;
-  } else if (D === 0) {
-      let x = - b / (2 * a);
-      arr.push(x);
-      return arr;
-    } else {
-        let x1 = (-b + Math.sqrt(D)) / (2 * a); 
-        let x2 = (-b - Math.sqrt(D)) / (2 * a);
-        arr.push(x1, x2);
-        return arr;
-      }
+  } else if (D > 0) {
+      let x1 = (-b + Math.sqrt(D)) / (2 * a); 
+      let x2 = (-b - Math.sqrt(D)) / (2 * a);
+      arr.push(x1, x2);
+      return arr;  
+    } 
+    return arr;
 };
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  if (parseInt(percent) === NaN) {
-    console.log (`'Параметр "percent" содержит неправильное значение "${percent}"'`);
-  }
-  if (parseInt(contribution) === NaN) {
-    console.log (`Параметр "contribution" содержит неправильное значение "${contribution}"`);
-  }
-  if (parseInt(contribution) === NaN) {
-    console.log (`Параметр "amount" содержит неправильное значение "${amount}"`);
-  }
-
-  let s =  (amount - contribution);
-  let p = (percent / 12);
-  let today = new Date();
-  let n = (13 - parseInt(today.getMonth())) + parseInt(date.getMonth()) + 1;
-  totalAmount = (s * (p + p / (((1 + p) ** n) - 1))); 
-    
-  return (+totalAmount.toFixed(2));
+  if (typeof percent !== Number) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (typeof contribution !== Number) {
+      return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+    } else if (typeof amount !== Number) {
+      return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+      } else {
+        let s =  (amount - contribution);
+        let p = (percent / 12 * 100);
+        let today = new Date();
+        let n = (14 -today.getMonth()) + date.getMonth();
+        totalAmount = (s * (p + p / (((1 + p) ** n) - 1))) * n; 
+          
+        return (+totalAmount.toFixed(2));
+      }    
 };
