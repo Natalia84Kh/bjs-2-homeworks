c53c9fc740ae99d0438da5582dcd030a377f1428
Natalia84Kh <73022006+Natalia84Kh@users.noreply.github.com>
2021-07-10 20:36:24 +0300

    Add files via upload
    second commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,19 +1,10 @@
 function compareArrays(arr1, arr2) {
-  let result = arr1.every(isEqual);
-  
-  function isEqual (arr1, arr2) {
-    for (let i = 0, let j = 0; i < arr1.length, j < arr2.length; i++,  j++) {
-      return arr1[i] === arr2[j];
-    }
-  }  
-  return result; // boolean  
+ 
+  return every(item, arr1) === every(item, arr2); // boolean  
 };
 
 function advancedFilter(arr) {
 
-  const arr1 = arr.filter((item) => item > 0);
-  const arr2 = arr1.filter((item) => item % 3 === 0);
-  const resultAr = arr2.map((item) => item * 10);
+  return arr.filter((item) => (item > 0) && (item % 3 === 0)).map((item) => item * 10);
 
-  return resultArr; // array
 }
