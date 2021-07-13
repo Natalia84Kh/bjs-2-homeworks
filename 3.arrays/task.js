3ff1f91f4a7e8ffa6e219c0a7a96099db190426a
Natalia84Kh <73022006+Natalia84Kh@users.noreply.github.com>
2021-07-13 21:44:43 +0300

    №6

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,8 +1,7 @@
 function compareArrays(arr1, arr2) {
-  if(arr1.length === arr2.length) {
-    return arr1.every((item, index) => item === arr2[index]); // boolean
-  }
-  return false;  
+ 
+    return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]); // boolean
+
 };
 
 function advancedFilter(arr) {
