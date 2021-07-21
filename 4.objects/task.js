37b73f8dc09c00fb276172ed2bda8059deea491d
Natalia84Kh <73022006+Natalia84Kh@users.noreply.github.com>
2021-07-21 08:15:11 +0300

    Add files via upload

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
-function Student(name, gender, age) {
-    // Ваш код
+function Student (name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;  // Ваш код
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName//ваш код
 }
+
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    [mark].push(mark);
+  }
+  return this.marks;
+}
+
+Student.prototype.addMarks = function (...marks) {
+  this.marks = [mark];
+  for (let mark of marks) {
+    [mark].push(mark);
+  }
+  return this.marks;
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (let mark of this.marks) {
+    sum += mark;
+  }
+  return sum / this.marks.length;
+}
+
+Student.prototype.exlude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.exluded = "Low grades";
+}
+
+// const student1 = new Student ("Daria", "female", 18);
+// const student2 = new Student ("Maria", "female", 40);
+
 
 // ваш код для остальных методов
